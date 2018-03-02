3fc58f8c2cd47de0be0fed5d466e89857b39e15c
juanpasq <manu_pasquali@hotmail.com>
2018-03-02 14:20:15 -0300

    conexion a fire DB comun en componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+    import { NgModule, ErrorHandler } from '@angular/core';
+    import { BrowserModule } from '@angular/platform-browser';
+    import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+    import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+    
+    apiKey: "AIzaSyB8j-winlTep0CYfmJS6GiWX-6kIwQhnzE",
+    authDomain: "takabet2018.firebaseapp.com",
+    databaseURL: "https://takabet2018.firebaseio.com",
+    projectId: "takabet2018",
+    storageBucket: "takabet2018.appspot.com",
+    messagingSenderId: "833580256701"    
+    
+};
 
 @NgModule({
   declarations: [
@@ -20,8 +35,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  //  AngularFireAuthModule,
+    BrowserModule,   
+    IonicModule.forRoot(MyApp),      
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+    
+    items: Observable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+      this.items = afDB.list('grupos').valueChanges();
 
   }
 
